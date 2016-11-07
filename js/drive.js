ff8d4b154cd1a91124b698810a20107ae88e9a21
Dirk Hesse <herr.dirk.hesse@gmail.com>
2016-11-07 13:41:00 +0100

    Preventing page reload.

--- js/drive.js
+++ js/drive.js
@@ -1,47 +1,57 @@
-function parseUrl() {
-    var urlparts = window.location.href.split("?");
-    return { base: urlparts[0],
-             slide: parseInt(urlparts[1] || "0") };
-}
+function makeDriver() {
 
-function uniKeyCode(event) {
-    var key = event.keyCode || event.which;
-    console.log("Unicode KEY code: " + key);
-    var func = {"39": nextSlide,
-                "37": previousSlide}[key];
-    func();
-}
+    function parseUrl() {
+        var urlparts = window.location.href.split("?");
+        return { base: urlparts[0],
+                 slide: parseInt(urlparts[1] || "0") };
+    }
 
-function getSlides() {
-    return document.getElementById("outer")
-        .getElementsByClassName("slide");
-}
+    var currentSlide = parseUrl().slide;
 
-function setVisibility() {
-    var urlInfo = parseUrl();
-    var slides = getSlides();
-    for (var i = 0; i < slides.length; i += 1){
-        if (i != urlInfo.slide) {
-            slides[i].style.display = "none";
+    function uniKeyCode(event) {
+        var key = event.keyCode || event.which;
+        console.log("Unicode KEY code: " + key);
+        var func = {"39": nextSlide,
+                    "37": previousSlide}[key];
+        func();
+    }
+
+    function getSlides() {
+        return document.getElementById("outer")
+            .getElementsByClassName("slide");
+    }
+
+    function setVisibility() {
+        var slides = getSlides();
+        for (var i = 0; i < slides.length; i += 1){
+            if (i != currentSlide)
+                slides[i].style.display = "none";
+            else
+                slides[i].style.display = "inline";
         }
     }
-}
 
-function nextSlide() {
-    var urlInfo = parseUrl();
-    var next = urlInfo.slide + 1;
-    if (next < getSlides().length)
-        window.location.href = urlInfo.base + "?" + next.toString();
-}
+    function nextSlide() {
+        var next = currentSlide + 1;
+        if (next < getSlides().length){
+            currentSlide = next;
+            console.log(next);
+            setVisibility();
+        }
+    }
 
-function previousSlide() {
-    var urlInfo = parseUrl();
-    var prev = urlInfo.slide - 1;
-    if (prev >= 0)
-        window.location.href = urlInfo.base + "?" + prev.toString();
+    function previousSlide() {
+        var prev = currentSlide - 1;
+        if (prev >= 0){
+            currentSlide = prev;
+            setVisibility();
+        }
+    }
+
+    setVisibility();
+    return uniKeyCode
 }
 
 window.onload = function() {
-    document.onkeypress = uniKeyCode;
-    setVisibility();
+    document.onkeypress = makeDriver();
 };
